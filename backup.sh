ce19ee1936c19ded3bebb7eede04e2cbf46622eb
Jon Chen <fly@sjchen.net>
2013-03-10 19:52:26 +0000

    single-server changes

--- backup.sh
+++ backup.sh
@@ -3,23 +3,22 @@
 # Minecraft Backup Script                  
 # Author: Mark Ide <cranstonide@gmail.com> 
 # Github: https://github.com/cranstonide/linux-minecraft-scripts
-hostname=`hostname`
 
 # Move into the directory with all Linux-Minecraft-Scripts
 cd "$( dirname $0 )"
 
 # Read configuration file
-if [ "$hostname" == "locke" ]
+if [ "$(whoami)" == "vanilla" ]
     then source mc-config.cfg
-elif [ "$hostname" == "voltaire" ]
+    elif [ "$(whoami)" == "minecraft" ]
     then source ftb-config.cfg
 fi
 
 tarDir=`basename $minecraftDir`
 
 # We need to first put the server in readonly mode to reduce the chance of backing up half of a chunk. 
-screen -p 0 -S voltairemc  -X eval "stuff \"save-off\"\015"
-screen -p 0 -S voltairemc -X eval "stuff \"save-all\"\015"
+screen -p 0 -S $(whoami) -X eval "stuff \"save-off\"\015"
+screen -p 0 -S $(whoami) -X eval "stuff \"save-all\"\015"
 
 # Wait a few seconds to make sure that Minecraft has finished backing up.
 sleep 5
@@ -33,15 +32,15 @@ cp -R $minecraftDir/* $localBUDest/$serverNick-most-recent
 
 # Create an archived copy in .tar.gz format.
 # rm -rf $localBUDest/$serverNick-$backupStamp.tar.gz
-tar -czf $localBUDest/$serverNick-$backupStamp.tar.gz -C /home/minecraft $tarDir/*
+tar -czf $localBUDest/$serverNick-$backupStamp.tar.gz -C $HOME/$tarDir/*
 rsync -a $localBUDest/$serverNick-$backupStamp.tar.gz backups:$remoteBUDest
 
 # Don't forget to take the server out of readonly mode.
-screen -p 0 -S voltairemc -X eval "stuff \"save-on\"\015"
+screen -p 0 -S $(whoami) -X eval "stuff \"save-on\"\015"
 
 # Wait a second for the gnu-screen to allow another stuffing and optionally alert users that the backup has been completed.
 sleep 1
-screen -p 0 -S voltairemc -X eval "stuff \"say Backup has been completed.\"\015"
+screen -p 0 -S $(whoami) -X eval "stuff \"say Backup has been completed.\"\015"
 
 # (Optionally) Remove all old (older than 7 days) backups to cut down on disk utilization. 
 find $localBUDest* -mtime +2 -exec rm {} -rf \;
