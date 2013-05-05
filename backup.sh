26a808696ee3b13115ce32ed3265c4a2c3400046
Jon Chen <fly@sjchen.net>
2013-05-05 03:50:44 +0000

    add full path to boto-rsync as its not in $PATH

--- backup.sh
+++ backup.sh
@@ -34,7 +34,7 @@ cp -R $minecraftDir/* $localBUDest/$serverNick-most-recent
 # rm -rf $localBUDest/$serverNick-$backupStamp.tar.gz
 nice tar -czf $localBUDest/$serverNick-$backupStamp.tar.gz -C $HOME/$tarDir/ .
 #cp $localBUDest/$serverNick-$backupStamp.tar.gz $tahoedir/
-boto-rsync -g public-read --endpoint objects.dreamhost.com $localBUDest/$serverNick-$backupStamp.tar.gz s3://voltairemc/$dodir
+/usr/local/bin/boto-rsync -g public-read --endpoint objects.dreamhost.com $localBUDest/$serverNick-$backupStamp.tar.gz s3://voltairemc/$dodir
 #rsync -a $localBUDest/$serverNick-$backupStamp.tar.gz backups:$remoteBUDest
 
 # Don't forget to take the server out of readonly mode.
