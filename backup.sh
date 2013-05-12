39928367bf16a7d7c8f5debc8eb2ee1a6ad9cd55
Jon Chen <fly@sjchen.net>
2013-05-12 05:58:34 +0000

    use google cloud storage instead of dreamobjects

--- backup.sh
+++ backup.sh
@@ -34,7 +34,7 @@ cp -R $minecraftDir/* $localBUDest/$serverNick-most-recent
 # rm -rf $localBUDest/$serverNick-$backupStamp.tar.gz
 nice tar -czf $localBUDest/$serverNick-$backupStamp.tar.gz -C $HOME/$tarDir/ .
 #cp $localBUDest/$serverNick-$backupStamp.tar.gz $tahoedir/
-/usr/local/bin/boto-rsync -g public-read --endpoint objects.dreamhost.com $localBUDest/$serverNick-$backupStamp.tar.gz s3://voltairemc/$dodir
+/usr/local/bin/boto-rsync -g public-read $localBUDest/$serverNick-$backupStamp.tar.gz gs://voltairemc/$dodir
 #rsync -a $localBUDest/$serverNick-$backupStamp.tar.gz backups:$remoteBUDest
 
 # Don't forget to take the server out of readonly mode.
@@ -46,4 +46,3 @@ screen -p 0 -S $(whoami) -X eval "stuff \"say Backup has been completed.\"\015"
 
 # (Optionally) Remove all old (older than 7 days) backups to cut down on disk utilization. 
 find $localBUDest* -mtime +2 -exec rm {} -rf \;
-
