fc1ca88e4ec36e5bddf53b3c7df620a45afc5e3d
Jon Chen <bsd@voltaire.sh>
2014-03-25 01:43:59 -0400

    remove commented code

--- backup.sh
+++ backup.sh
@@ -31,13 +31,9 @@ mkdir $localBUDest/$serverNick-most-recent
 cp -R $minecraftDir/* $localBUDest/$serverNick-most-recent
 
 # Create an archived copy in .tar.gz format.
-# rm -rf $localBUDest/$serverNick-$backupStamp.tar.gz
 nice tar -czf $localBUDest/$serverNick-$backupStamp.tar.gz -C $HOME/$tarDir/ .
-#cp $localBUDest/$serverNick-$backupStamp.tar.gz $tahoedir/
-#/usr/local/bin/boto-rsync -g public-read $localBUDest/$serverNick-$backupStamp.tar.gz gs://voltairemc/$dodir
 /srv/minecraft/bu/env/bin/boto-rsync -g public-read --endpoint objects.dreamhost.com $localBUDest/$serverNick-$backupStamp.tar.gz s3://voltairemc/$dodir
 /srv/minecraft/bu/env/bin/boto-rsync -g public-read $localBUDest/$serverNick-$backupStamp.tar.gz gs://bsdlpmc/$dodir
-#rsync -a $localBUDest/$serverNick-$backupStamp.tar.gz backups:$remoteBUDest
 
 # Don't forget to take the server out of readonly mode.
 screen -p 0 -S $(whoami) -X eval "stuff \"save-on\"\015"
